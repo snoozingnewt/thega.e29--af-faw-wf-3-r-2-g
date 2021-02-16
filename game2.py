8d6809467c28c130bd0b36df572f5e8fbee7c6b2
snoozingnewt <78986317+snoozingnewt@users.noreply.github.com>
2021-02-15 21:21:49 -0500

    Update game2.py
    Added scene 0 and enemy thing

--- game2.py
+++ game2.py
@@ -4,6 +4,10 @@ import time
 global itemstate
 global scene
 global scene
+global enemytype
+global life
+global enemydamage
+global enemyhealth
 #Displays the choices
 def choices():
     print('''    Press 1 to start a new game.
@@ -42,9 +46,11 @@ def choices():
         choices()
     
 def secondchoices():
+    global life
     print('''    Press 1 to continue.
     Press 2 to quit.
     Press 3 to get your gamestate and itemstate.
+    Press 4 to get your health.
     ''')
     secondchoice = int(input())
     if secondchoice == 1:
@@ -55,6 +61,12 @@ def secondchoices():
     elif secondchoice == 3:
         print("Gamestate: "+str(scene)+". Itemstate: "+str(itemstate))
         secondchoices()
+    elif secondchoice == 4:
+        print(life)
+        secondchoices()
+    else:
+        print("Error.")
+        secondchoices()
 
 def thirdchoices():
     num1 = 2
@@ -64,9 +76,9 @@ def thirdchoices():
     global itemstate
     global scene
     global scene
-    print('''    Enter your gamestate.''')
+    print('''    Enter your gamestate. (A number)''')
     thirdchoice = int(input())
-    print('''    Enter your itemstate.''')
+    print('''    Enter your itemstate. (A number)''')
     fourthchoice = int(input())
 
 #game
@@ -111,15 +123,24 @@ def scenes():
     num3 = 5
     num4 = 7
     global scene
+    global itemstate
     if scene == 0:
         time.sleep(0.5)
-        print("hi world 1")
+        print("Welcome to the tutorial.")
+        print("You have different items. Armor, Sword, Staff, and if you win the game, you get an OP pass that will allow you to kill any enemy.")
+        print("Here's a sword. You can try checking your itemstate now.")
+        print("You now have a starter sword. It does 5 damage.")
+        itemstate = 2
+        secondchoices()
+        print("An enemy approaches!")
+        enemy()
+        
         scene = num1
         secondchoices()
         scenes()
     if scene == num1:
         time.sleep(0.5)
-        print("hi world 2")
+        print("Not Bad.")
         scene = num2
         secondchoices()
         scenes()
@@ -136,10 +157,80 @@ def scenes():
 
     
     
+def enemy():
+    global enemytype
+    global enemyhealth
+    global reset
+    if scene == 0:
+        enemytype = 1
+    #---
+    if enemytype == 1:
+        global enemydamage
+        print("You are challenged by... Training Dummy!")
+        enemyhealth = 10
+        enemydamage = 0
+        attack()
+        
+        
+def attack():
+    
+    num1 = 2
+    num2 = 3
+    num3 = 5
+    num4 = 7
+    global life
+    global enemyhealth
+    global reset
+    global scene
+    mode = input("Press a to attack, r to restart, and d to passively regain health.")
+    if enemyhealth < 0 or enemyhealth == 0:
+            scene = scene + num1
+            scenes()
+    if mode == 'a':
+        if itemstate % num1 == 0:
+            enemyhealth = enemyhealth-5
+            print("Enemyhealth: "+ str(enemyhealth))
+            counterattack()
+    if mode == 'r':
+        life = maxlife
+        reset += 1
+        enemy()
+    if mode == 'd':
+        life = life+liferegain
+        lifetest()
+        print("Enemyhealth: "+ str(enemyhealth))
+        counterattack()
+        
+def lifetest():
+    global life
+    if life > maxlife:
+        life = life-1
+        lifetest()
+    else:
+        pass
 
-
-
-
+def counterattack():
+    global scene
+    global enemydamage
+    global life
+    num1 = 2
+    num2 = 3
+    num3 = 5
+    num4 = 7
+    if enemyhealth < 0 or enemyhealth == 0:
+            scene = scene + num1
+            scenes()
+    life = life - enemydamage
+    if life == 0 or life < 0:
+        battleloss()
+    else:
+        attack()
+        
+def battleloss():
+    print("You lose... try again?")
+    deaths += 1
+    scenes()
+    
 class actions:
     def __init__ (self, money, life, itemstate, scene):
         self.life = life
@@ -151,11 +242,15 @@ class actions:
     global scene
 
 
-
+reset = 0
 money = 0
+liferegain = 5
+maxlife = 100
 life = 100
 itemstate = 1
 scene = 0
+deaths = 0
 choices()
+enemytype = 0
 
 
